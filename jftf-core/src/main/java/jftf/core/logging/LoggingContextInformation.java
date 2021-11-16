5cb4891e3f784bc64ce814622e72b33dd176a441
decemyn <radulescudecebal@outlook.com>
2021-11-16 23:03:24 +0200

    JFTF-6 - Logger factory implementation

--- jftf-core/src/main/java/jftf/core/logging/LoggingContextInformation.java
+++ jftf-core/src/main/java/jftf/core/logging/LoggingContextInformation.java
@@ -1,12 +1,68 @@
 package jftf.core.logging;
 
+import ch.qos.logback.classic.Level;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
+
 public class LoggingContextInformation {
-    private final String currentApplicationID;
-    public LoggingContextInformation(String currentApplicationID) {
+    public final static Level debugLogLevel = Level.DEBUG;
+    public final static Level infoLogLevel = Level.INFO;
+    public final static Level errorLogLevel = Level.ERROR;
+    public final static Level defaultLogLevel = Level.INFO;
+    public final static String consoleAppender = "consoleAppender";
+    private final static String consoleAppenderConfiguration = "logback.xml";
+    public final static String fileAppender = "fileAppender";
+    private final static String fileAppenderConfiguration = "logback_file.xml";
+    public final static String defaultAppender = consoleAppender;
+    private final static String defaultAppenderConfiguration = consoleAppenderConfiguration;
+    public final static ArrayList<Level> validLogLevels = new ArrayList<>(Arrays.asList(debugLogLevel,infoLogLevel,errorLogLevel));
+    public final static ArrayList<String> validAppenders = new ArrayList<>(Arrays.asList(consoleAppender,fileAppender));
+    private String currentApplicationID = "";
+    private Level baseLogLevel = defaultLogLevel;
+    private String baseAppender = defaultAppender;
+
+    public LoggingContextInformation(String currentApplicationID, Level baseLogLevel, String baseAppender) {
+        this.currentApplicationID = currentApplicationID;
+        if(validLogLevels.contains(baseLogLevel))
+            this.baseLogLevel = baseLogLevel;
+        if(validAppenders.contains(baseAppender)){
+            this.baseAppender = baseAppender;
+        }
+    }
+
+    public LoggingContextInformation(Level baseLogLevel, String baseAppender) {
+        if(validLogLevels.contains(baseLogLevel))
+            this.baseLogLevel = baseLogLevel;
+        if(validAppenders.contains(baseAppender)){
+            this.baseAppender = baseAppender;
+        }
+    }
+
+    public String getApplicationID() {
+        return this.currentApplicationID;
+    }
+
+    public void setApplicationID(String currentApplicationID){
         this.currentApplicationID = currentApplicationID;
     }
 
-    public String getCurrentApplicationID() {
-        return currentApplicationID;
+    public Level getLogLevel(){
+        return this.baseLogLevel;
     }
+
+    public String getAppender(){
+        return this.baseAppender;
+    }
+
+    public String getAppenderConfiguration(){
+        if(Objects.equals(this.baseAppender, LoggingContextInformation.consoleAppender))
+            return LoggingContextInformation.consoleAppenderConfiguration;
+        else if (Objects.equals(this.baseAppender, LoggingContextInformation.fileAppender))
+            return LoggingContextInformation.fileAppenderConfiguration;
+        else
+            return LoggingContextInformation.defaultAppenderConfiguration;
+    }
+
 }
--- jftf-core/src/main/java/jftf/core/logging/LoggingController.java
+++ jftf-core/src/main/java/jftf/core/logging/LoggingController.java
@@ -1,22 +1,215 @@
 package jftf.core.logging;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.classic.joran.JoranConfigurator;
+import ch.qos.logback.core.joran.spi.JoranException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ch.qos.logback.classic.LoggerContext;
+
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class LoggingController {
-    private final Logger LOGGER;
-    private final LoggerContext currentLoggerContext;
-    public LoggingController(LoggingContextInformation currentLoggerContextInformation) {
+    private Logger LOGGER = null;
+    private ch.qos.logback.classic.Logger rootLogger = null;
+    private LoggingContextInformation currentLoggerContextInformation;
+    private LoggerContext currentLoggerContext = null;
+    private boolean faultMode = false;
+    private final List<String> faultModeErrorMessages = new ArrayList<>();
+    private static LoggingController loggerInstance;
+    private static boolean internalLogs = false;
+
+    private LoggingController(LoggingContextInformation currentLoggerContextInformation) {
         try {
-            this.LOGGER = LoggerFactory.getLogger(currentLoggerContextInformation.getCurrentApplicationID());
-            this.currentLoggerContext = (LoggerContext) this.LOGGER;
+            if(!Objects.equals(currentLoggerContextInformation.getApplicationID(), ""))
+                this.LOGGER = LoggerFactory.getLogger(currentLoggerContextInformation.getApplicationID());
+            else {
+                this.LOGGER = LoggerFactory.getLogger(LoggingController.class.getName());
+                currentLoggerContextInformation.setApplicationID(LoggingController.class.getName());
+            }
+            this.currentLoggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
+            this.rootLogger = this.currentLoggerContext.getLogger(Logger.ROOT_LOGGER_NAME);
+            this.currentLoggerContextInformation = currentLoggerContextInformation;
+            this.shiftToAttachedContextInformation();
         }
         catch(Exception e){
-            throw new LoggerFailedInitialization(String.format("Logging context for application ID: '%s' failed to initialize!",currentLoggerContextInformation.getCurrentApplicationID()),e);
+            this.switchToFaultMode(e);
         }
-        this.LOGGER.info(String.format("Registered new logging context for application ID: '%s'", currentLoggerContextInformation.getCurrentApplicationID()));
     }
-    public Logger exposeLogger(){
-        return this.LOGGER;
+
+    public static LoggingController LoggerFactory(LoggingContextInformation currentLoggerContextInformation){
+        if(loggerInstance == null)
+            loggerInstance = new LoggingController(currentLoggerContextInformation);
+        return loggerInstance;
     }
+
+    public static LoggingController LoggerFactory(LoggingContextInformation currentLoggerContextInformation, boolean internalLogs){
+        if(loggerInstance == null) {
+            LoggingController.internalLogs = internalLogs;
+            loggerInstance = new LoggingController(currentLoggerContextInformation);
+        }
+        return loggerInstance;
+    }
+
+    public static void disableInternalLogs(){
+        internalLogs = false;
+    }
+
+    public  static void enableInternalLogs(){
+        internalLogs = true;
+    }
+
+    public void LogDebug(String logMessage){
+        if(!this.faultMode){
+            this.LOGGER.debug(logMessage);
+        }
+    }
+
+    public void LogInfo(String logMessage){
+        if(!this.faultMode){
+            this.LOGGER.info(logMessage);
+        }
+    }
+
+    public void LogError(String logMessage){
+        if(!this.faultMode){
+            this.LOGGER.error(logMessage);
+        }
+    }
+
+    public void LogToMinimumLogLevel(String logMessage){
+        if(!this.faultMode){
+            if(this.rootLogger.getLevel() == LoggingContextInformation.debugLogLevel)
+                this.LogDebug(logMessage);
+            else if(this.rootLogger.getLevel() == LoggingContextInformation.infoLogLevel)
+                this.LogInfo(logMessage);
+            if(this.rootLogger.getLevel() == LoggingContextInformation.errorLogLevel)
+                this.LogError(logMessage);
+        }
+    }
+
+    private void announceContextRegistration(){
+        if(!this.faultMode){
+            if(internalLogs)
+                this.LogToMinimumLogLevel(String.format("Shifted to attached logging context for application ID: '%s' | Log level: '%s' | Appender type: '%s (INTERNAL / IGNORE)'", this.currentLoggerContextInformation.getApplicationID(),this.currentLoggerContextInformation.getLogLevel().levelStr,this.currentLoggerContextInformation.getAppender()));
+        }
+    }
+
+    private void announceNewContextRegistration(LoggingContextInformation newContextInformation){
+        if(!this.faultMode){
+            if(internalLogs)
+                this.LogToMinimumLogLevel(String.format("Shifted to new logging context for application ID: '%s' | Log level: '%s' | Appender type: '%s' (INTERNAL / IGNORE)", this.currentLoggerContextInformation.getApplicationID(),newContextInformation.getLogLevel().levelStr,newContextInformation.getAppender()));
+        }
+    }
+
+    private void switchToFaultMode(Exception e){
+        this.faultMode = true;
+        StringWriter sw = new StringWriter();
+        PrintWriter pw = new PrintWriter(sw);
+        e.printStackTrace(pw);
+        this.faultModeErrorMessages.add(sw.toString());
+    }
+
+    private void printFaultErrorMessages(){
+        if(this.faultMode)
+            System.out.println(this.faultModeErrorMessages);
+        else
+            System.out.println("Logger not currently in fault mode!");
+    }
+
+    private void shiftToContextInformationLogLevel(){
+        if(!this.faultMode){
+            if(this.rootLogger.getLevel() != this.currentLoggerContextInformation.getLogLevel()) {
+                if(internalLogs)
+                    this.LogInfo(String.format("Shifting log level for root logger: '%s' --> '%s' (INTERNAL / IGNORE)", this.rootLogger.getLevel().levelStr, this.currentLoggerContextInformation.getLogLevel().levelStr));
+                try {
+                    this.rootLogger.setLevel(this.currentLoggerContextInformation.getLogLevel());
+                }
+                catch(Exception e){
+                    this.switchToFaultMode(e);
+                }
+                if(internalLogs)
+                    this.LogToMinimumLogLevel(String.format("Shifted to --> '%s' Logging level shift confirmation message! (INTERNAL / IGNORE)",this.rootLogger.getLevel().levelStr));
+            }
+        }
+    }
+
+    private void shiftToLogLevel(LoggingContextInformation newContextInformation){
+        if (!this.faultMode) {
+            if (this.rootLogger.getLevel() != newContextInformation.getLogLevel()) {
+                if(internalLogs)
+                    this.LogToMinimumLogLevel(String.format("Shifting log level for logging context : '%s' --> '%s' (INTERNAL / IGNORE)", this.rootLogger.getLevel().levelStr, newContextInformation.getLogLevel()));
+                try {
+                    this.rootLogger.setLevel(newContextInformation.getLogLevel());
+                } catch (Exception e) {
+                    this.switchToFaultMode(e);
+                }
+                if(internalLogs)
+                    this.LogToMinimumLogLevel(String.format("Shifted to --> '%s' Logging level shift confirmation message! (INTERNAL / IGNORE)",this.rootLogger.getLevel().levelStr));
+            }
+        }
+    }
+
+    private void shiftToContextInformationAppender(){
+        if(!this.faultMode){
+            JoranConfigurator jc = new JoranConfigurator();
+            jc.setContext(this.currentLoggerContext);
+            this.currentLoggerContext.reset();
+            InputStream is = LoggingController.class.getClassLoader().getResourceAsStream(this.currentLoggerContextInformation.getAppenderConfiguration());
+            try {
+                jc.doConfigure(is);
+                this.announceContextRegistration();
+            }
+            catch (JoranException e){
+                this.switchToFaultMode(e);
+            }
+        }
+    }
+
+    private void shiftToAppender(LoggingContextInformation newContextInformation){
+        if(!this.faultMode){
+            JoranConfigurator jc = new JoranConfigurator();
+            jc.setContext(this.currentLoggerContext);
+            this.currentLoggerContext.reset();
+            InputStream is = LoggingController.class.getClassLoader().getResourceAsStream(newContextInformation.getAppenderConfiguration());
+            try {
+                jc.doConfigure(is);
+                this.announceNewContextRegistration(newContextInformation);
+            }
+            catch (JoranException e){
+                this.switchToFaultMode(e);
+            }
+        }
+    }
+
+    public void shiftToAttachedContextInformation(){
+        if(!this.faultMode) {
+            if(internalLogs)
+                this.LogToMinimumLogLevel(String.format("Shifting to attached context information! Log level: '%s' | Appender type: '%s' (INTERNAL / IGNORE)",this.currentLoggerContextInformation.getLogLevel(),this.currentLoggerContextInformation.getAppender()));
+            this.shiftToContextInformationAppender();
+            this.shiftToContextInformationLogLevel();
+        }
+    }
+
+    public void shiftToNewContextInformation(LoggingContextInformation newContextInformation){
+        if(!this.faultMode) {
+            if(internalLogs)
+                this.LogToMinimumLogLevel(String.format("Shifting to new logging context information! Log level: '%s' | Appender type: '%s' (INTERNAL / IGNORE)",newContextInformation.getLogLevel(),newContextInformation.getAppender()));
+            this.shiftToAppender(newContextInformation);
+            this.shiftToLogLevel(newContextInformation);
+        }
+    }
+
+    public void attachNewContextInformation(LoggingContextInformation newContextInformation){
+        if(!this.faultMode) {
+            newContextInformation.setApplicationID(this.currentLoggerContextInformation.getApplicationID());
+            if(internalLogs)
+                this.LogToMinimumLogLevel(String.format("New logging context information attached to logger! Log level: '%s' | Appender type: '%s' (INTERNAL / IGNORE)",newContextInformation.getLogLevel(),newContextInformation.getAppender()));
+            this.currentLoggerContextInformation = newContextInformation;
+        }
+    }
+
 }
